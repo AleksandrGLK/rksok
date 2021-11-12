4b9bb4f398014ac03b82a0d4c414f11654873b8f
aglk <alksndrglk@gmail.com>
2021-11-12 20:17:03 +0300

    RKSOK Class

--- rksok.py
+++ rksok.py
@@ -25,74 +25,87 @@ VERB_TO_FUNCTION = {
 
 ENCODING = "UTF-8"
 
-storage = LocalDirectoryStorage()
 
+class RKSOK:
+    def __init__(self):
+        self.storage = LocalDirectoryStorage()
 
-async def check_message(message):
-    reader, writer = await asyncio.open_connection("vragi-vezde.to.digital", 51624)
-    writer.write(message.encode(ENCODING))
-    data = await reader.read(1024)
-    writer.close()
-    return data
+    @staticmethod
+    async def check_message(data: bytes) -> bytes:
+        reader, writer = await asyncio.open_connection("vragi-vezde.to.digital", 51624)
+        message = strings.PREFIX + data
+        writer.write(message.encode(ENCODING))
+        data = await reader.read(1024)
+        writer.close()
+        return data
 
-
-async def data_handling(verb, *args):
-    if not hasattr(storage, VERB_TO_FUNCTION.get(verb)) or args[0].isspace():
-        raise RequestDoesNotMeetTheStandart
-    function = getattr(storage, VERB_TO_FUNCTION.get(verb))
-    return await function(*args[: len(inspect.signature(function).parameters.keys())])
-
-
-def parce_response(data):
-    return re.sub(strings.MESSAGE_PATTERN, r"\1|\2|\3", data, count=1).split("|")
-
-
-async def data_manipulations(raw_data):
-
-    data = raw_data.decode(ENCODING)
-    command, name_field, information = parce_response(data)
-    if len(name_field) > 30:
-        return strings.INCORRECT_REQUEST
-
-    message = strings.PREFIX + data
-    server_response = await check_message(message)
-
-    if server_response.decode(ENCODING).startswith(
-        f"{RegulatoryServerResponseStatus.APPROVED.value} "
-    ):
-        for verb in RequestVerb:
-            if command.startswith(verb.value):
-                break
-        storage_response = await asyncio.gather(
-            data_handling(verb, name_field, information), return_exceptions=True
+    async def data_handling(self, verb, *args):
+        if not hasattr(self.storage, VERB_TO_FUNCTION.get(verb)) or args[0].isspace():
+            raise RequestDoesNotMeetTheStandart
+        function = getattr(self.storage, VERB_TO_FUNCTION.get(verb))
+        return await function(
+            *args[: len(inspect.signature(function).parameters.keys())]
         )
-        response = storage_response[0]
-        if isinstance(response, Exception):
+
+    @staticmethod
+    def parce_response(data: str) -> list:
+        return re.sub(strings.MESSAGE_PATTERN, r"\1|\2|\3", data, count=1).split("|")
+
+    async def data_manipulations(self, raw_data: bytes) -> str:
+
+        data = raw_data.decode(ENCODING)
+        command, name_field, information = RKSOK.parce_response(data)
+        if len(name_field) > 30 :
+            return strings.INCORRECT_REQUEST
+
+        server_response = await RKSOK.check_message(data)
+
+        if server_response.decode(ENCODING).startswith(
+            f"{RegulatoryServerResponseStatus.APPROVED.value} "
+        ):
+            for verb in RequestVerb:
+                if command.startswith(verb.value):
+                    break
+            storage_response = await asyncio.gather(
+                self.data_handling(verb, name_field, information),
+                return_exceptions=True,
+            )
+            response = storage_response[0]
+            if isinstance(response, Exception):
+                response = strings.INCORRECT_REQUEST
+            return response
+
+    async def read_message(self, reader):
+        message = b""
+        while True:
+            data = await reader.read(1024)
+            if not data:
+                break
+            message += data
+        if not message.endswith(b'\r\n\r\n'):
+            raise RequestDoesNotMeetTheStandart
+        return message
+
+    async def handle_echo(self, reader, writer) -> None:
+        try:
+            raw_data = await asyncio.wait_for(self.read_message(reader), timeout=10)
+            response = await self.data_manipulations(raw_data)
+        except (asyncio.exceptions.TimeoutError, RequestDoesNotMeetTheStandart) as e:
             response = strings.INCORRECT_REQUEST
-        return response
+
+        writer.write(response.encode(ENCODING))
+        await writer.drain()
+        writer.close()
+
+    async def __call__(self) -> None:
+        server = await asyncio.start_server(self.handle_echo, "127.0.0.1", 8888)
+
+        addrs = ", ".join(str(sock.getsockname()) for sock in server.sockets)
+        print(f"Serving on {addrs}")
+
+        async with server:
+            await server.serve_forever()
 
 
-async def handle_echo(reader, writer):
-    try:
-        raw_data = await asyncio.wait_for(reader.readuntil(separator=b"\r\n\r\n"), 10)
-    except asyncio.IncompleteReadError as e:
-        raw_data = e.partial
-
-    response = await data_manipulations(raw_data)
-
-    writer.write(response.encode(ENCODING))
-    await writer.drain()
-    writer.close()
-
-
-async def main():
-    server = await asyncio.start_server(handle_echo, "127.0.0.1", 8888)
-
-    addrs = ", ".join(str(sock.getsockname()) for sock in server.sockets)
-    print(f"Serving on {addrs}")
-
-    async with server:
-        await server.serve_forever()
-
-
-asyncio.run(main(), debug=True)
+rksok = RKSOK()
+asyncio.run(rksok(), debug=True)
