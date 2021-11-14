1179fb5e932888eb95a9692e3a10043b88307d58
aglk <alksndrglk@gmail.com>
2021-11-14 13:30:01 +0300

    Some config + logging

--- core/config.py
+++ core/config.py
@@ -1,2 +1,4 @@
+SERVER_ADDRESS = ("127.0.0.1", 7777)
+PERMISSION_SERVER = ("vragi-vezde.to.digital", 51624)
 ENCODING = "UTF-8"
 MESSAGE_PATTERN = r"(.*?)\s(.*?|\s+)(?=\s\w{5}/1.0).*\s(.*)[.\r\n]*"
--- rksok.py
+++ rksok.py
@@ -1,11 +1,13 @@
 import asyncio
 import re
+import sys
 
 from collections import namedtuple
 from typing import NamedTuple
 
 from resources import strings
 from core import config
+from core.logging import logger
 from storage.repositories import STORAGE_FACTORY
 from errors.validation_error import RequestDoesNotMeetTheStandart, EmptyNameField
 from resources.conditions import (
@@ -26,8 +28,9 @@ class RKSOK:
 
     @staticmethod
     async def check_message(data: bytes) -> str:
+        logger.info("Connecting to {} port {}".format(*config.PERMISSION_SERVER))
         """Sends sequest to verify the possibility of processing client requests."""
-        reader, writer = await asyncio.open_connection("vragi-vezde.to.digital", 51624)
+        reader, writer = await asyncio.open_connection(*config.PERMISSION_SERVER)
         message = strings.PREFIX + data
         writer.write(message.encode(config.ENCODING))
         data = await reader.read(1024)
@@ -53,33 +56,47 @@ class RKSOK:
                 if command == verb.value:
                     break
             else:
+                logger.exception("Can't parse client message")
                 raise RequestDoesNotMeetTheStandart
             RKSOK.name_validation(name_field)
+            logger.info(
+                "Message parsed and validated \n command:{} name:{} info:{} verb:{}".format(
+                    command, name_field, information, verb
+                )
+            )
             return Message(command, name_field, information, verb)
         except ValueError:
+            logger.exception("Can't parse client message")
             raise RequestDoesNotMeetTheStandart
 
     @staticmethod
     def name_validation(name: str) -> None:
         """Client name size validation."""
+        logger.info("Name validation")
         if not name or name.isspace():
+            logger.exception("Failed name is empty: Name validation")
             raise EmptyNameField
         elif len(name) > 30:
+            logger.exception("Failed by length: Name validation")
             raise RequestDoesNotMeetTheStandart
 
     async def parse_server_response(
         self, server_response: str, message: NamedTuple
     ) -> str:
         """Message processing from the check server."""
+        logger.info("Parsing server response")
         if not server_response.startswith(
             f"{RegulatoryServerResponseStatus.APPROVED.value} "
         ):
+            logger.info("Permisson Denied by regulatory service")
             return server_response
+        logger.info("Data handling request")
         storage_response = await asyncio.gather(
             self.data_handling(message),
             return_exceptions=True,
         )
         response = storage_response[0]
+        logger.info("Return result: %s", response)
         if isinstance(response, Exception):
             response = strings.INCORRECT_REQUEST
         return response
@@ -106,7 +123,10 @@ class RKSOK:
     async def rksok_server(self, reader, writer) -> None:
         """RKSOK server function."""
         try:
+            logger.info("Waiting for message")
             client_message = await self.read_message(reader)
+            logger.info("Received message %s", client_message)
+            logger.info("Processing message ...")
             server_response, message = await self.client_message_processing(
                 client_message
             )
@@ -121,7 +141,8 @@ class RKSOK:
         writer.close()
 
     async def __call__(self) -> None:
-        server = await asyncio.start_server(self.rksok_server, "127.0.0.1", 7777)
+        logger.info("Connecting to {} port {}".format(*config.SERVER_ADDRESS))
+        server = await asyncio.start_server(self.rksok_server, *config.SERVER_ADDRESS)
         addrs = ", ".join(str(sock.getsockname()) for sock in server.sockets)
         print(f"Serving on {addrs}")
         async with server:
