7fe6dd1bac47aa363e51b03e632f0348527f7472
aglk <alksndrglk@gmail.com>
2021-11-17 19:34:16 +0300

    New message parser

--- core/config.py
+++ core/config.py
@@ -9,4 +9,4 @@ STORAGE_FACTORY = {
 SERVER_ADDRESS = ("127.0.0.1", 7777)
 PERMISSION_SERVER = ("vragi-vezde.to.digital", 51624)
 ENCODING = "UTF-8"
-MESSAGE_PATTERN = r"(.*?)\s(.*?|\s+)(?=\s\w{5}\/1\.0).*\s{2}([\s\S]*?)\s{4}$"
+MESSAGE_PATTERN = r"(.*?)\s(.*?|\s+)(?=\s\w{5}\/1\.0).*[\s{2}]?([\s\S]*?)\s{4}$"
